e5de8e4d84faa475ff37ef198ce21be2a05b6435
Josh <jrscorca@gmail.com>
2025-03-04 13:05:22 -0800

    Initial Commit

---
+++ EnvoyCoffee/EnvoyCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvoyCoffeeApp.swift
+//  EnvoyCoffee
+//
+//  Created by Joshua Scorca on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct EnvoyCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
